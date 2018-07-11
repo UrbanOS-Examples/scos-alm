63b897e696bea1fd530c45fe1916b545df3431ee
Kyle Smith <kylesmith@pillartechnology.com>
2018-07-11 16:15:52 -0400

    SMRT-208 - remove ded code
    co-authored-by: Jessie Morris <jmorris2@pillartechnology.com>

--- jenkins_relay.tf
+++ jenkins_relay.tf
@@ -57,19 +57,6 @@ resource "aws_route53_record" "github-webhook" {
   records = ["${aws_instance.jenkins_relay.public_ip}"]
 }
 
-resource "aws_acm_certificate" "cert" {
-  domain_name       = "ci-webhook.${var.environment}.smartcolumbusos.com"
-  validation_method = "DNS"
-}
-
-resource "aws_route53_record" "cert_validation" {
-  name    = "${aws_acm_certificate.cert.domain_validation_options.0.resource_record_name}"
-  type    = "${aws_acm_certificate.cert.domain_validation_options.0.resource_record_type}"
-  zone_id = "${aws_route53_zone.smartcolumbusos_hosted_zone.zone_id}"
-  records = ["${aws_acm_certificate.cert.domain_validation_options.0.resource_record_value}"]
-  ttl     = 60
-}
-
 resource "aws_acm_certificate_validation" "cert" {
   certificate_arn         = "${aws_acm_certificate.cert.arn}"
   validation_record_fqdns = ["${aws_route53_record.cert_validation.fqdn}"]
