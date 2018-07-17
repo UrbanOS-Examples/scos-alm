67ff76d0a9e8afebdf9bd289ac95e180a88cb130
Kyle Smith <kyle@kmaismith.com>
2018-07-17 10:49:36 -0400

    SMRT-174 various fixes (#58)
    * move ecr module from alm/ecr to modules/ecr
    
    * SMRT-174 fix broken reference to ecr module
    
    - streaming_service_smoke_test_repository
    
    * SMRT-174 Environment deploys have key name output
    
    - vpc_peering remote state now references backend config instead of having a duplicate config in the tfvars
    
    * SMRT-174 Make all env environment configs consistent
    
    * SMRT-174 - ${workspace} -> ${terraform.workspace}
    
    co-authored-by: NAME <YOUR@EMAIL.com>
    
    * SMRT-174 - Revert terraform_remote_state referencing backend config
    
    co-authored-by: NAME <YOUR@EMAIL.com>

--- container_registry.tf
+++ container_registry.tf
@@ -1,26 +1,26 @@
 module "joomla_restore_repository" {
-  source = "ecr"
+  source = "../modules/ecr"
 
   alm_account_id  = "${var.alm_account_id}"
   repository_name = "joomla-restore"
 }
 
 module "joomla_cron_repository" {
-  source = "ecr"
+  source = "../modules/ecr"
 
   alm_account_id  = "${var.alm_account_id}"
   repository_name = "joomla-cron"
 }
 
 module "joomla_nginx_repository" {
-  source = "ecr"
+  source = "../modules/ecr"
 
   alm_account_id  = "${var.alm_account_id}"
   repository_name = "joomla-nginx"
 }
 
 module "streaming_service_smoke_test_repository" {
-  source = "ecr"
+  source = "../modules/ecr"
 
   alm_account_id  = "${var.alm_account_id}"
   repository_name = "streaming-service-smoke-test"
