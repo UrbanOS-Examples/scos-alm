7858f34816cef74b5f8a8738f93dd4b069123d67
Jeff Grunewald <jeff@grunewalddesign.com>
2018-11-07 14:44:18 -0500

    Smrt-666 Integrate LDAP with Ranger (#287)
    * SMRT-666: Adding a ranger database
    
    co-authored-by: Kyle Smith <kylesmith@pillartechnology.com>
    
    * SMRT-666 Adding ldap connection via terraform
    
    co-authored-by: Kyle Smith <kylesmith@pillartechnology.com>
    
    * Fixing cluster template json, ldap connection string, and ranger-admin site settings
    
    * LDAP for ranger correctly configured with cloud storage
    
    * SMRT-666 Ranger manages hive, yarn, and hdfs by default
    
    co-authored-by: Kyle Smith <kylesmith@pillartechnology.com>
    
    * SMRT-666 Moving passwords into secrets and retrieving their secretsmanager IDs
    
    co-authored-by: Kyle Smith <kylesmith@pillartechnology.com>

--- iam.tf
+++ iam.tf
@@ -1,15 +1,26 @@
 module "iam_stack" {
-  source              = "../modules/iam"
-  vpc_id              = "${module.vpc.vpc_id}"
-  subnet_ids          = ["${module.vpc.private_subnets}"]
-  ssh_key             = "${aws_key_pair.cloud_key.key_name}"
-  management_cidr     = "${var.vpc_cidr}"
-  realm_cidr          = "10.0.0.0/8"
-  iam_hostname_prefix = "iam"
-  zone_id             = "${aws_route53_zone.public_hosted_zone.zone_id}"
-  zone_name           = "${aws_route53_zone.public_hosted_zone.name}"
-  realm_name          = "internal.smartcolumbusos.com"
-  vpc_cidr            = "${var.vpc_cidr}"
+  source                = "../modules/iam"
+  vpc_id                = "${module.vpc.vpc_id}"
+  subnet_ids            = ["${module.vpc.private_subnets}"]
+  ssh_key               = "${aws_key_pair.cloud_key.key_name}"
+  management_cidr       = "${var.vpc_cidr}"
+  realm_cidr            = "10.0.0.0/8"
+  iam_hostname_prefix   = "iam"
+  zone_id               = "${aws_route53_zone.public_hosted_zone.zone_id}"
+  zone_name             = "${aws_route53_zone.public_hosted_zone.name}"
+  realm_name            = "${var.kerberos_realm_name}"
+  vpc_cidr              = "${var.vpc_cidr}"
+  freeipa_replica_count = "${var.freeipa_replica_count}"
+}
+
+variable "kerberos_realm_name" {
+  description = "The name of the kerberos realm under management in domain format"
+  default     = "internal.smartcolumbusos.com"
+}
+
+variable "freeipa_replica_count" {
+  description = "The number of freeipa replicas to deploy"
+  default     = 1
 }
 
 output "freeipa_server_ips" {
@@ -18,4 +29,8 @@ output "freeipa_server_ips" {
 
 output "keycloak_server_ip" {
   value = "${module.iam_stack.keycloak_server_ip}"
+}
+
+output "bind_user_password_secret_id" {
+  value = "${module.iam_stack.bind_user_password_secret_id}"
 }
--- variables/sandbox.tfvars
+++ variables/sandbox.tfvars
@@ -25,3 +25,7 @@ alm_role_arn = "arn:aws:iam::068920858268:role/admin_role"
 alm_state_bucket_name = "scos-sandbox-terraform-state"
 
 key_pair_public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDRAvH6k7iEeRDz9SQtkH1o8KiUaed/e2hmzTUjA8bhmeWVLPsgCMKIUKR0jdPlvdZ0AmMLXswobsXj08dPgWxUZxoAWIqKCjd969KckydUpBtcb+X2Q+tpOcugBOQSY1H8hgNrdcRKEaUllTfvseJ9pBOYU7j9VuZ608HQhfZw7+aS8wi9o/BJwejtpWdlo6gkxXoIRqDX/ioYg+W6Tc7yoUzAEANwZAy3/3GKWDrh+9jnzR6mEEN48Nuee49wWfP5G0T/v4+Gvux5zioHb3rcmmR9YTkFOiv1poInhXlPdc7Q38yj+z6E+hACNN3rK80YjU0ByaSPltPjqm9ZYmPX"
+
+freeipa_replica_count = 0
+
+kerberos_realm_name = "sandbox.internal.smartcolumbusos.com"
