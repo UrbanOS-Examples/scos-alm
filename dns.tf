88334f49970fab5227a8056f9349c8f35328253f
rajones-hntb <rajones@hntb.com>
2018-11-15 13:01:56 -0500

    Add ALM and ALM-durable hosted zone names to the terraform output (#307)
    co-authored-by: Kyle Smith <kylesmith@pillartechnology.com>

--- dns.tf
+++ dns.tf
@@ -34,3 +34,7 @@ output "name_servers" {
 output "public_hosted_zone_id" {
   value = "${aws_route53_zone.public_hosted_zone.zone_id}"
 }
+
+output "public_hosted_zone_name" {
+  value = "${aws_route53_zone.public_hosted_zone.name}"
+}
