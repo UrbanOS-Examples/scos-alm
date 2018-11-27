f4a84e453f977068b5e7235e7397586be4f4cc25
Remorc <bcromer07@gmail.com>
2018-11-27 16:19:00 -0500

    SMRT-743 Set secret retention policy to 0 in pre-prod environments (#330)
    co-authored-by: Brandon Cromer <bcromer@pillartechnology.com>

--- iam.tf
+++ iam.tf
@@ -1,16 +1,17 @@
 module "iam_stack" {
-  source                = "../modules/iam"
-  vpc_id                = "${module.vpc.vpc_id}"
-  subnet_ids            = ["${module.vpc.private_subnets}"]
-  ssh_key               = "${aws_key_pair.cloud_key.key_name}"
-  management_cidr       = "${var.vpc_cidr}"
-  realm_cidr            = "10.0.0.0/8"
-  iam_hostname_prefix   = "iam"
-  zone_id               = "${aws_route53_zone.public_hosted_zone.zone_id}"
-  zone_name             = "${aws_route53_zone.public_hosted_zone.name}"
-  realm_name            = "${var.kerberos_realm_name}"
-  vpc_cidr              = "${var.vpc_cidr}"
-  freeipa_replica_count = "${var.freeipa_replica_count}"
+  source                  = "../modules/iam"
+  vpc_id                  = "${module.vpc.vpc_id}"
+  subnet_ids              = ["${module.vpc.private_subnets}"]
+  ssh_key                 = "${aws_key_pair.cloud_key.key_name}"
+  management_cidr         = "${var.vpc_cidr}"
+  realm_cidr              = "10.0.0.0/8"
+  iam_hostname_prefix     = "iam"
+  zone_id                 = "${aws_route53_zone.public_hosted_zone.zone_id}"
+  zone_name               = "${aws_route53_zone.public_hosted_zone.name}"
+  realm_name              = "${var.kerberos_realm_name}"
+  vpc_cidr                = "${var.vpc_cidr}"
+  freeipa_replica_count   = "${var.freeipa_replica_count}"
+  recovery_window_in_days = "${var.recovery_window_in_days}"
 }
 
 variable "kerberos_realm_name" {
@@ -21,6 +22,11 @@ variable "kerberos_realm_name" {
 variable "freeipa_replica_count" {
   description = "The number of freeipa replicas to deploy"
   default     = 1
+}
+
+variable "recovery_window_in_days" {
+  description = "How long to allow secrets to be recovered if they are deleted"
+  default     = 30
 }
 
 output "freeipa_server_ips" {
