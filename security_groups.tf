a2d2a59a387243418ae7cc3e9709762c8f85c475
rajones@hntb.com <rajones@hnbt.com>
2020-01-13 14:43:01 -0500

    tighten up the open ingress ports on sg for jenkins

--- security_groups.tf
+++ security_groups.tf
@@ -22,8 +22,38 @@ resource "aws_security_group_rule" "chatter_ingress_self" {
   protocol                 = "-1"
   security_group_id        = "${aws_security_group.chatter.id}"
   source_security_group_id = "${aws_security_group.chatter.id}"
-  from_port                = 0
-  to_port                  = 65535
+  from_port                = 80
+  to_port                  = 80
+  type                     = "ingress"
+}
+
+resource "aws_security_group_rule" "chatter_ingress_self" {
+  description              = "Allow nodes to communicate with each other."
+  protocol                 = "-1"
+  security_group_id        = "${aws_security_group.chatter.id}"
+  source_security_group_id = "${aws_security_group.chatter.id}"
+  from_port                = 443
+  to_port                  = 443
+  type                     = "ingress"
+}
+
+resource "aws_security_group_rule" "chatter_ingress_self" {
+  description              = "Allow nodes to communicate with each other."
+  protocol                 = "-1"
+  security_group_id        = "${aws_security_group.chatter.id}"
+  source_security_group_id = "${aws_security_group.chatter.id}"
+  from_port                = 50000
+  to_port                  = 50000
+  type                     = "ingress"
+}
+
+resource "aws_security_group_rule" "chatter_ingress_self" {
+  description              = "Allow nodes to communicate with each other."
+  protocol                 = "-1"
+  security_group_id        = "${aws_security_group.chatter.id}"
+  source_security_group_id = "${aws_security_group.chatter.id}"
+  from_port                = 8080
+  to_port                  = 8080
   type                     = "ingress"
 }
 
