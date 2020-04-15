8a1eb3acf0eb382216e2f92b543bb5f3d5cafebb
Benjamin Brewer <ben@benbrewer.me>
2020-04-15 09:55:48 -0400

    bump to include even more plugin updates

--- jenkins.tf
+++ jenkins.tf
@@ -306,7 +306,7 @@ locals {
   jenkins_port    = 8080
   jnlp_port       = 50000
   service_name    = "jenkins_master"
-  service_image   = "scos/jenkins-master:d394737d5e80cae9b723b7d6c254b67135bdc6a4"
+  service_image   = "scos/jenkins-master:8b5955ebd1b3e13362e6350f899022ed4d8a15d3"
   service_command = []
   directory_name  = "jenkins_home"
 }
